aae7f0f4a42e8d4d8a4d2bd1f0f36e656f586e34
Hieu Nguyen <nn1678008@gmail.com>
2023-06-29 01:27:08 +0700

    first commit

---
+++ Project/DA_LTTQ/DA_LTTQ/DataConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace DA_LTTQ
+{
+    class DataConnection
+    {
+        string conStr;
+        public DataConnection()
+        {
+            conStr = @"Data Source=DESKTOP-3Q5V10H;Initial Catalog=DA_LTTQ;user id=sa;password=Nguyenduchieu2003#";
+        }
+
+        public SqlConnection getConnect()
+        {
+            return new SqlConnection(conStr);
+        }
+    }
+}
